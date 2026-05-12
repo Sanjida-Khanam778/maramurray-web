16f63738310542e98ea40d6a653ad79db8faa015
Sanjida Khanam <sanjida.ice.778@gmail.com>
2026-05-12 11:05:22 +0600

    preloader added

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,3 +1,6 @@
+import { useState, useEffect } from 'react'
+import Lottie from 'lottie-react'
+import preloaderAnimation from '../../../public/florlejsonloitte.json'
 import Banner from '../../components/Banner/Banner'
 import HowItWorks from '../../components/HowItWorks/HowItWorks'
 import ChooseYourPath from '../../components/ChooseYourPath/ChooseYourPath'
@@ -9,6 +12,26 @@ import TestimonialsSection from '../../components/TestimonialsSection/Testimonia
 import VideoTutorialSection from '../../components/VideoTutorialSection/VideoTutorialSection'
 
 export default function Home() {
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    // Hide preloader after 2.5 seconds
+    const timer = setTimeout(() => {
+      setLoading(false);
+    }, 4500);
+    return () => clearTimeout(timer);
+  }, []);
+
+  if (loading) {
+    return (
+      <div className="fixed inset-0 z-50 flex items-center justify-center bg-[#F8F8F8]">
+        <div className="w-screen flex items-center justify-center">
+          <Lottie animationData={preloaderAnimation} loop={true} className="w-full h-full object-cover" />
+        </div>
+      </div>
+    );
+  }
+
   return (
     <div className='font-bricolage bg-[#F8F8F8]'>
       <Banner />
